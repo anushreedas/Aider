1302076505f06965b96a05194a507afadae30a6b
Anushree Das <anushreedas.2710@gmail.com>
2019-01-25 01:07:01 +0530

    Main Window Design Complete

--- Aider/MainView.xaml.cs
+++ Aider/MainView.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Media.Animation;
 
 namespace Aider
 {
@@ -23,6 +24,48 @@ namespace Aider
         public MainView()
         {
             InitializeComponent();
+            
+        }
+
+        private void SettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            ShowHideMenu("sbShowRightMenu", SettingsPanel);
+            SettingsButton.IsEnabled = false;
+        }
+
+                
+        private void CancelSettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            ShowHideMenu("sbHideRightMenu", SettingsPanel);
+            SettingsButton.IsEnabled = true;
+        }
+
+        private void ShowHideMenu(string Storyboard, Panel pnl)
+        {
+            Storyboard sb = Resources[Storyboard] as Storyboard;
+            pnl.Visibility = System.Windows.Visibility.Visible;
+            sb.Begin(pnl);
+
+            if (Storyboard.Contains("Show"))
+            {                
+                
+            }
+
+            if (Storyboard.Contains("Hide"))
+            {
+                
+            }
+
+        }
+
+        private void AttachmentsButton_Click(object sender, RoutedEventArgs e)
+        {
+            if((AttachmentsPanel.Visibility==System.Windows.Visibility.Collapsed) || (AttachmentsPanel.Margin == new Thickness(0, 0, 0, -100)) )
+                ShowHideMenu("sbShowBottomMenu", AttachmentsPanel);
+            else
+            {
+                ShowHideMenu("sbHideBottomMenu", AttachmentsPanel);
+            }
         }
     }
 }
