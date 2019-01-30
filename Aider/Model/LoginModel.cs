55a4479e4d377c42e2dd5a2971590a45b980361c
Shonit Bagmar <shonitbagmar@gmail.com>
2019-01-30 16:47:05 +0530

    commands

---
+++ Aider/Model/LoginModel.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Data.OracleClient;
+
+namespace Aider.Model
+{
+    public class LoginModel : INotifyPropertyChanged
+    {
+        public string userName;
+        public string password;
+        public string validation;
+
+        public string UserName
+        {
+            get
+            {
+                return userName;
+            }
+            set
+            {
+                if (userName != null)
+                {
+                    userName = value;
+                    OnPropertyChanged("UserName");
+                }
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                if (password != null)
+                {
+                    password = value;
+                    OnPropertyChanged("Password");
+                }
+            }
+        }
+
+
+        public string Validation
+        {
+            get
+            {
+                return validation;
+            }
+            set
+            {
+                string ora = "DATA SOURCE=localhost:1521/orcl.168.117.131;PERSIST SECURITY INFO=True;USER ID=ABC;PASSWORD=ROOT";
+                using (OracleConnection myConnection = new OracleConnection(ora))
+                {
+                    myConnection.Open();
+
+                    string cmdstr = "SELECT * FROM test";
+
+                    OracleCommand cmd = new OracleCommand(cmdstr, myConnection);
+
+                    OracleDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+
+                    }
+
+                }
+            }
+        }
+
+        public void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ Aider/ViewModel/LoginViewModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using Aider.Model;
+using System.Windows;
+using System.Windows.Input;
+using Aider.Command;
+
+namespace Aider.ViewModel
+{
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        private LoginModel _LoginModel;
+
+
+
+        public LoginModel Login
+        {
+            get
+            {
+                return login;
+            }
+            set
+            {
+                login = value;
+                NotifyPropertyChanged("Login");
+            }
+        }
+
+        private ObserICommand _LogInCommand;
+        public ICommand LogInCommand
+        {
+            get
+            {
+                if (_LogInCommand == null)
+                {
+                    _LogInCommand = new RelayCommand(LogInExecute, CanLogInExecute, false);
+                }
+                return _LogInCommand;
+            }
+        }
+
+        private void LogInExecute(object parameter)
+        {
+            // add loginin 
+        }
+
+        private bool CanLogInExecute(object parameter)
+        {
+            if (string.IsNullOrEmpty(_LoginModel.UserName) || string.IsNullOrEmpty(_LoginModel.Password))
+            {
+                return false;
+                // if user enter null values in the text boxes then it return false
+            }
+
+            else
+            {
+                //logic of username and password comparison
+                return true;
+            }
+        }
+
+    }
+}
